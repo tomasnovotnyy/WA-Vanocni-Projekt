d65ade6f7bcb66a326cb018ddb1bab9445e1e276
Tomáš Novotný <84340580+tomasnovotnyy@users.noreply.github.com>
2024-01-20 15:40:25 +0100

    Update logme.php

--- login/logme.php
+++ login/logme.php
@@ -1,28 +1,50 @@
 <?php 
 
+// Kontrola, zda byla stránka načtena pomocí POST metody
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                    
+    // Získání emailu a hesla z POST dat
     $email = $_POST['email'];
     $password = $_POST['password'];
 
+    // Inicializace proměnných pro kontrolu úspěšnosti přihlášení
     $successNext = TRUE;
     $emailVisibility = FALSE;
+
+    // Název souboru s daty
     $file_name = 'data'. '.json';
 
+    // Načtení aktuálních dat ze souboru
     $current_data = file_get_contents("$file_name");
+
+    // Převedení dat z JSONu do pole
     $array = json_decode($current_data, TRUE);
 
+    // Procházení pole s daty
     foreach ($array as $insecure_val) {
+
+        // Kontrola, zda se email nachází v datech a není prázdný
         if($insecure_val['email'] == $email && $insecure_val['email'] != '') {
             $emailVisibility = TRUE;
-            if($insecure_val['password'] == $password && $insecure_val['password'] != ''){
+
+            // Kontrola, zda heslo odpovídá hashi v datech a není prázdné
+            if(password_verify($password, $insecure_val['password']) && $insecure_val['password'] != ''){
                 $successNext = FALSE;
+
+                // Zahájení session a uložení emailu do session
                 session_start();
                 $_SESSION["email"]=$email;
+
+                // Přesměrování na chat.php
                 header("Location: /chat.php");
                 break;
             }
-        }header("Location: /index.php");
+        }
+    }
+
+    // Pokud se nepodařilo přihlásit, přesměrování na index.php
+    if($successNext){
+        header("Location: /index.php");
     }
 }
  
