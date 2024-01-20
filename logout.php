fa30d3cf81b6835ece655d6ceb1738ff57725ec4
Tomáš Novotný <84340580+tomasnovotnyy@users.noreply.github.com>
2024-01-20 15:43:04 +0100

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,13 @@
 <?php
+// Zahájení session
 session_start();
+
+// Zničení všech dat v session
 session_destroy();
+
+// Přesměrování na index.php
 header("Location: index.php");
+
+// Ukončení skriptu
 exit();
 ?>
